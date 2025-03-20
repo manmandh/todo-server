a72dd1512514accc42e49883288355d183bf67c4
manmandh <manmantran169@gmail.com>
2025-03-21 03:19:09 +0700

    fix: body parser

--- index.ts
+++ index.ts
@@ -1,35 +1,37 @@
-import bodyParser from 'body-parser'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import express from 'express'
-import mongoose from 'mongoose'
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
 
-import { MONGOOSE_URI, PORT } from './utils'
-import { userRoutes } from './routes/user.route'
-import { productRoutes } from './routes'
+import { MONGOOSE_URI, PORT } from "./utils";
+import { userRoutes } from "./routes/user.route";
+import { productRoutes } from "./routes";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(cookieParser())
-app.use(bodyParser.json())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.use('/users', userRoutes)
-app.use('/products', productRoutes)
+app.use("/users", userRoutes);
+app.use("/products", productRoutes);
 
 mongoose
-	.connect(MONGOOSE_URI)
-	.then(() => {
-		console.log('Connected to MongoDB')
-		app.listen(PORT, () => {
-			console.log(`SERVER IS RUNNING IN PORT: ${PORT} at ${new Date().toLocaleString()}`)
-		})
-	})
-	.catch(err => {
-		console.log(err)
-	})
+  .connect(MONGOOSE_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(
+        `SERVER IS RUNNING IN PORT: ${PORT} at ${new Date().toLocaleString()}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
